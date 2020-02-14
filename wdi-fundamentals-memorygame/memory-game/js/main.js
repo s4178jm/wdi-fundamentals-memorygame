876b6a9ab01549c3555db43a02febeccf81cc213
s4178jm <60681747+s4178jm@users.noreply.github.com>
2020-02-14 01:21:25 -0800

    Add files via upload

--- wdi-fundamentals-memorygame/memory-game/js/main.js
+++ wdi-fundamentals-memorygame/memory-game/js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+
+
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+let cardOne = cards [0];
+let cardTwo = cards [2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console .log("User flipped queen");
+console .log("User flipped king");
+if (cardsInPlay[0] === cardsInPlay[1] ){
+		alert("You found a match!");
+	}	else {
+		alert("Sorry, try again.");
+	}
