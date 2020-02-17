02aaf6e69d577c1d589100f02a48b6a11ad45ecc
s4178jm <60681747+s4178jm@users.noreply.github.com>
2020-02-16 22:58:40 -0800

    Add files via upload

--- wdi-fundamentals-memorygame/memory-game/js/main.js
+++ wdi-fundamentals-memorygame/memory-game/js/main.js
@@ -1,16 +1,44 @@
 console.log("Up and running!");
 
 
-const cards = ["queen","queen","king","king"];
+const cards = [
+{
+	rank:"queen",
+	suit:"hearts",
+	cardImage:"images/queen-of-hearts.png"
+},
+{
+	rank:"queen",
+	suit:"diamonds",
+	cardImage:"images/queen-of-diamonds.png"
+},
+{
+	rank:"king",
+	suit:"hearts",
+	cardImage:"images/king-of-hearts.png"
+},
+{
+	rank:"king",
+	suit:"diamonds",
+	cardImage:"images/king-of-diamonds.png"
+}
+];
 const cardsInPlay = [];
-let cardOne = cards [0];
-let cardTwo = cards [2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console .log("User flipped queen");
-console .log("User flipped king");
-if (cardsInPlay[0] === cardsInPlay[1] ){
-		alert("You found a match!");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
 	}	else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
+}
+function flipcard(cardID){
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+	cardsInPlay.push(cards[cardID].rank);
+	checkForMatch();
+};
+
+flipcard(0);
+flipcard(1);
+
